799e16c01e902fcde72d7ccb1d1d7fd1aa566bb8
Shreya Ravichandiran <sudhashreya07@gmail.com>
2025-03-22 22:55:49 +0530

    Reverse Linked List
    Runtime : 0 ms
    Memory : 42.43 MB

---
+++ ReverseLinkedList.java
@@ -0,0 +1,28 @@
+class Solution {
+    private ListNode head;
+    
+    public static class ListNode{
+        final int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }//added this to prevent the errors!
+        public ListNode reverseList(ListNode head) {
+            ListNode current = head;
+            ListNode previous = null;
+            ListNode next = null;
+            if(head==null){
+                return head;
+            }
+            while(current!=null){
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+            return previous;
+    }
+}
